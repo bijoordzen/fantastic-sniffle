76f15d510ce01bcf5d5512c68f54316203479d0e
Srinivas Bijoor <37250383+bijoordzen@users.noreply.github.com>
2026-09-20 00:39:39 +0530

    dbsetup
    dbsetup application.properties

---
+++ brinjal/src/main/java/com/example/brinjal/BrinjalApplication.java
@@ -0,0 +1,13 @@
+package com.example.brinjal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrinjalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrinjalApplication.class, args);
+	}
+
+}
---
+++ brinjal/src/test/java/com/example/brinjal/BrinjalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.brinjal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrinjalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
